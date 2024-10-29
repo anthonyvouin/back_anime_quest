f0e07acac9d91f67b7467a54f817d46ea24d52a5
anthony vouin <avouin@normandiewebschool.fr>
2024-10-29 11:28:39 +0100

    add

--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -2,8 +2,8 @@ import { FastifyInstance } from 'fastify';
 import { getCategories, getCategoryById, getMoviesByCategory, createCategory } from '../controllers/categoriesController';
 
 export default async function categoryRoutes(fastify: FastifyInstance) {
-	fastify.get('/categories', getCategories);
-	fastify.get('/categories/:id', getCategoryById);
-	fastify.get('/categories/:id/movies', getMoviesByCategory);
-	fastify.post('/categories', createCategory);
+	fastify.get('/api/categories', getCategories);
+	fastify.get('/api/categories/:id', getCategoryById);
+	fastify.get('/api/categories/:id/movies', getMoviesByCategory);
+	fastify.post('/api/categories', createCategory);
 }
